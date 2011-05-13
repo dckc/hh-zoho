76e510414556e21c5413a70d4719db478ee2cda6
Dan Connolly <http://www.madmode.com/>
2011-05-13 13:23:09 -0500

    db_tweaks.py for dabble to zoho tweaks

---
+++ db_tweaks.py
@@ -0,0 +1,70 @@
+
+import sqlite3
+import os
+from contextlib import contextmanager
+import csv
+
+def main(argv):
+    db, fn = argv[1:3]
+    table = os.path.basename(fn)[:-len(".csv")]
+    conn = sqlite3.connect(db)
+    with transaction(conn) as work:
+        import_csv(work, fn, table)
+
+
+def import_csv(trx, fn, table, colsize=500):
+    '''Import data from a comma-separated file into a table.
+
+    1st line contains column names; '_' is substituted for spaces.
+    '''
+    rows = csv.reader(open(fn))
+    colnames = [n.replace(' ', '_').upper()
+                for n in rows.next()]
+    trx.execute(_create_ddl(table, colnames, colsize))
+    trx.executemany(_insert_dml(table, colnames),
+                    [dict(zip(colnames, row))
+                     for row in rows])
+
+def _create_ddl(table, colnames, colsize):
+    '''
+    >>> _create_ddl('item', ('id', 'size', 'price'), 50)
+    'create table item ("id" varchar2(50), "size" varchar2(50), "price" varchar2(50))'
+    '''
+    return 'create table %s (%s)' % (
+        table, ', '.join(['"%s" varchar2(%d)' % (n, colsize)
+                          for n in colnames]))
+
+
+def _insert_dml(table, colnames):
+    '''
+    >>> _insert_dml('item', ('id', 'size', 'price'))
+    'insert into item ("id", "size", "price") values (:id, :size, :price)'
+    '''
+    return 'insert into %s (%s) values (%s)' %(
+        table,
+        ', '.join(['"%s"' % n for n in colnames]),
+        ', '.join([':' + n
+                   for n in colnames]))
+                                               
+
+@contextmanager
+def transaction(conn):
+    '''Return an Oracle database cursor manager.
+
+    :param conn: an Oracle connection
+    '''
+    c = conn.cursor()
+    try:
+        yield c
+    except sqlite3.Error:
+        conn.rollback()
+        raise
+    else:
+        conn.commit()
+    finally:
+        c.close()
+
+
+if __name__ == '__main__':
+    import sys
+    main(sys.argv)
