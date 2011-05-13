194a374a9a52326a29b8d2ed94be492138d25076
Dan Connolly <http://www.madmode.com/>
2011-05-13 14:06:26 -0500

    figured out how to get a Zoho API ticket

--- migrate_hh.py
+++ migrate_hh.py
@@ -3,13 +3,46 @@ import sqlite3
 import os
 from contextlib import contextmanager
 import csv
+from urllib2 import urlopen
+from urllib import urlencode
+import getpass
 
 def main(argv):
+    u = argv[1]
+    def pw_cb():
+        return getpass.getpass('Password for %s: ' % u)
+    print zoho_api_ticket(u, pw_cb)
+
+
+def main_import_csv(argv):
     db, fn = argv[1:3]
     table = os.path.basename(fn)[:-len(".csv")]
     conn = sqlite3.connect(db)
     with transaction(conn) as work:
         import_csv(work, fn, table)
+
+
+def zoho_api_ticket(login_id, password_cb,
+                    ticket_file=',zoho_ticket',
+                    api_addr='https://accounts.zoho.com/login',
+                    servicename='ZohoCreator'):
+    # cf https://api.creator.zoho.com/Creator-API-Prerequisites-Generate-a-Ticket.html
+    try:
+        body = open(ticket_file).read()
+    except IOError:
+        params = {'LOGIN_ID': login_id,
+                  'PASSWORD': password_cb(),
+                  'FROM_AGENT': 'true',
+                  'servicename': servicename,
+                  'submit': 'Generate Ticket'}
+        ans = urlopen(api_addr, urlencode(params))
+        body = ans.read()
+        open(ticket_file, "w").write(body)
+    return [v for n, v in
+            [parts for parts in
+             [l.split('=', 1) for l in body.split('\n')]
+             if len(parts) == 2]
+            if n == 'TICKET'][0]
 
 
 def import_csv(trx, fn, table, colsize=500):
